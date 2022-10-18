c831a5f59b109dc62dea842221d3947f4bc27673
kyle.lesack@gmail.com <kyle.lesack@gmail.com>
2022-10-17 22:15:54 -0600

    removed directories that have been replaced in real data and simulated data directories

--- fusorsv_longread/00_scripts/0_prepare_data/0_clean_files.sh
+++
@@ -1,5 +0,0 @@
-rm -r 1_processed_beds
-rm -r 2_overlap_analysis
-rm -r 3_summaries
-rm *.log
-rm *.sh
--- shortread/00_scripts/1_analyze_overlap/6_create_upset_plots.sh
+++
@@ -1,3 +0,0 @@
-python3 00_scripts/2_make_plots/create_upset_plots.py 00_scripts/2_make_plots/config_files/deletion_high_confidence-cnv.conf deletion --max_combos 20
-python3 00_scripts/2_make_plots/create_upset_plots.py 00_scripts/2_make_plots/config_files/duplication_high_confidence-cnv.conf duplication --max_combos 20
-python3 00_scripts/2_make_plots/create_upset_plots.py 00_scripts/2_make_plots/config_files/inversion_high_confidence-cnv.conf inversion --max_combos 20
